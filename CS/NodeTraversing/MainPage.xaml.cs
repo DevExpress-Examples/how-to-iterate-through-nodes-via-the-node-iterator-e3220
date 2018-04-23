f2ad41b492259f68ee710aeb4ffd56bc169b3cd8
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/NodeTraversing/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using DevExpress.Xpf.Grid;
+
+namespace NodeTraversing {
+    public partial class MainPage : UserControl {
+        public MainPage() {
+            InitializeComponent();
+            grid.ItemsSource = Stuff.GetStuff();
+        }
+
+        private void SmartExpandNodes(int minChildCount) {
+            TreeListNodeIterator nodeIterator = new TreeListNodeIterator(treeListView.Nodes, true);
+            while (nodeIterator.MoveNext())
+                nodeIterator.Current.IsExpanded = nodeIterator.Current.Nodes.Count >= minChildCount;
+        }
+
+        private void grid_Loaded(object sender, RoutedEventArgs e) {
+            SmartExpandNodes(4);
+        }
+    }
+}
---
+++ VB/NodeTraversing/MainPage.xaml.vb
@@ -0,0 +1,34 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Net
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Animation
+Imports System.Windows.Shapes
+Imports DevExpress.Xpf.Grid
+
+Namespace NodeTraversing
+	Partial Public Class MainPage
+		Inherits UserControl
+		Public Sub New()
+			InitializeComponent()
+			grid.ItemsSource = Stuff.GetStuff()
+		End Sub
+
+		Private Sub SmartExpandNodes(ByVal minChildCount As Integer)
+			Dim nodeIterator As New TreeListNodeIterator(treeListView.Nodes, True)
+			Do While nodeIterator.MoveNext()
+				nodeIterator.Current.IsExpanded = nodeIterator.Current.Nodes.Count >= minChildCount
+			Loop
+		End Sub
+
+		Private Sub grid_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			SmartExpandNodes(4)
+		End Sub
+	End Class
+End Namespace
